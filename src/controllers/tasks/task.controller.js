ff2a70298fed4a2bae099e130a4425206b25a223
OSL28 <olynexdevteam@gmail.com>
2024-08-13 10:46:55 +0600

    gettask updated to task controller

--- src/controllers/tasks/task.controller.js
+++ src/controllers/tasks/task.controller.js
@@ -1,5 +1,4 @@
 const TasksModel = require('../../models/tasks.model');
-
 exports.addTask = async (req, res) => {
   const body = req.body; // req to frontend
   try {
@@ -10,30 +9,23 @@ exports.addTask = async (req, res) => {
     res.status(500).send({ message: 'Task Adding Error!', error });
   }
 };
-
 // get all tasks with pagination
 exports.getTasks = async (req, res) => {
   try {
-    const { page = 1, limit = 10 } = req.query; 
+    const { page = 1, limit = 10 } = req.query;
     const skip = (page - 1) * limit; // Calculate the number of documents to skip
-
     // Get the total count of tasks
     const totalTasks = await TasksModel.countDocuments();
-
     // Find tasks with pagination
-    const tasks = await TasksModel.find()
-      .skip(skip)  
-      .limit(parseInt(limit)); 
-
+    const tasks = await TasksModel.find().skip(skip).limit(parseInt(limit));
     // Calculate total pages
     const totalPages = Math.ceil(totalTasks / limit);
-
     // Send the paginated tasks with total pages
     res.status(200).send({
       tasks,
-      totalPages
+      totalPages,
     });
   } catch (err) {
-    res.status(500).send({ message: "Failed to retrieve tasks", err });
+    res.status(500).send({ message: 'Failed to retrieve tasks', err });
   }
 };
