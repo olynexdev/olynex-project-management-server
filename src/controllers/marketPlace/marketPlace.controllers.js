17ed2001b0fdecf4edf2a692a5f7fc2cd8d0e617
safi islam <118913244+safiislam@users.noreply.github.com>
2024-08-19 13:13:08 +0600

    marketplace  setup

---
+++ src/controllers/marketPlace/marketPlace.controllers.js
@@ -0,0 +1,46 @@
+const MarketPlaceModel = require("../../models/marketPlace.model");
+
+exports.addMarketPlace = async (req, res) => {
+  const body = req.body; // req to frontend
+  try {
+    const result = await MarketPlaceModel.create(body);
+    res.status(201).send(result);
+  } catch (error) {
+    res.status(500).send({ message: "MarketPlace Insert Error!", error });
+  }
+};
+
+exports.getAllMarketPlace = async (req, res) => {
+  try {
+    const result = await MarketPlaceModel.find();
+    res.status(201).send(result);
+  } catch (error) {
+    res.status(500).send({ message: "MarketPlace Insert Error!", error });
+  }
+};
+
+exports.deleteMarketPlace = async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  try {
+    const result = await MarketPlaceModel.deleteOne({ _id: id });
+    res.status(200).send(result);
+  } catch (err) {
+    res
+      .status(500)
+      .send({ message: "Error deleting Market Place", error: err });
+  }
+};
+exports.updateMarketPlace = async (req, res) => {
+  const { id } = req.params;
+  const updateDoc = req.body;
+  try {
+    const result = await MarketPlaceModel.updateOne(
+      { _id: id },
+      { $set: updateDoc }
+    );
+    res.status(200).send(result);
+  } catch (err) {
+    res.status(500).send({ message: "Error Edit Market Place", error: err });
+  }
+};
