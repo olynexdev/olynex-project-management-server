824de554e79185b8882b79c50a608a2f10f5a5b9
Your Name <olynexdev@gmail.com>
2024-08-20 17:51:12 +0600

    get single user with user id

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,6 +9,7 @@ const {
   getUsers,
   getUserwithEmail,
   getUserWithRole,
+  getUserbyUserId,
 } = require("../controllers/users/getUser.controller");
 
 const router = express.Router();
@@ -21,5 +22,6 @@ router.delete("/delete-user/:id", deleteUser); // delete a User
 router.get("/get-user", getUserwithEmail);
 router.get("/get-user-role/:email", getUserWithRole);
 router.get("/get-user/:id", getUserById);
+router.get('/get-user-by-user-id/:userId', getUserbyUserId)
 
 module.exports = router;
