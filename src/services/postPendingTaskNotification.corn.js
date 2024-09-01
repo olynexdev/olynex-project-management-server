879f58bd3cd90ff1da07bb63e8b4d75607d402d6
OSL28 <olynexdevteam@gmail.com>
2024-09-01 09:24:13 +0600

    updated post pending task notification corn jobs

--- src/services/postPendingTaskNotification.corn.js
+++ src/services/postPendingTaskNotification.corn.js
@@ -3,20 +3,18 @@ const TaskModel = require('../models/tasks.model');
 const NotificationModel = require('../models/notification.model');
 const UserModel = require('../models/users.model');
 
-const schedulePendingTaskCheck = io => {
-  cron.schedule('* * * * *', async () => {
-    try {
-      // Find tasks where the status is still pending
-      const pendingTasks = await TaskModel.find({
-        'approvalChain.status': 'pending',
-      });
+const schedulePendingTaskNotification = (task, io) => {
+  const taskCreatedAt = new Date(task.taskCreatedAt);
+  const notificationTime = new Date(taskCreatedAt.getTime() + 10 * 60000); // 10 minutes later
 
-      // Iterate over each pending task
-      for (const task of pendingTasks) {
+  const job = cron.schedule(
+    `${notificationTime.getMinutes()} ${notificationTime.getHours()} * * *`,
+    async () => {
+      try {
         const findEmployee = task.approvalChain.find(
           user => user.designation === 'employee'
         );
-        const userProfile = await UserModel.find({
+        const userProfile = await UserModel.findOne({
           userId: findEmployee.userId,
         });
 
@@ -25,7 +23,7 @@ const schedulePendingTaskCheck = io => {
           const notificationInfo = {
             taskId: task._id,
             senderId: findEmployee.userId,
-            senderProfile: userProfile[0]?.userProfile,
+            senderProfile: userProfile?.userProfile,
             receiverId: task.taskCreator.userId,
             text: `${findEmployee.name} has not accepted the task ${task.taskId} yet!`,
             isRead: false,
@@ -37,7 +35,6 @@ const schedulePendingTaskCheck = io => {
 
           // Emit the notification using Socket.IO
           if (io) {
-            // Emit the notification using Socket.IO
             io.to(notificationInfo.receiverId).emit(
               'receiveNotification',
               notificationInfo
@@ -47,6 +44,30 @@ const schedulePendingTaskCheck = io => {
             console.error('Socket.io instance is not defined.');
           }
         }
+
+        // Stop the job after execution
+        job.stop();
+      } catch (error) {
+        console.error('Error sending task notification:', error);
+      }
+    },
+    { scheduled: true }
+  );
+
+  job.start();
+};
+
+const schedulePendingTaskCheck = io => {
+  cron.schedule('* * * * *', async () => {
+    try {
+      // Find tasks where the status is still pending
+      const pendingTasks = await TaskModel.find({
+        'approvalChain.status': 'pending',
+      });
+
+      // Iterate over each pending task
+      for (const task of pendingTasks) {
+        schedulePendingTaskNotification(task, io);
       }
     } catch (error) {
       console.error('Error running cron job for task notifications:', error);
