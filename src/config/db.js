af105a7991ad8e8eec3a981f3e61d78090a43bfa
Your Name <olynexdev@gmail.com>
2024-09-02 13:03:42 +0600

    change in main

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,27 @@
-// src/config/db.js
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+// Load environment variables from .env file
 dotenv.config();
 
+// MongoDB connection function
 const connectDB = async () => {
   try {
+    // Check if MONGO_URI is defined
+    if (!process.env.MONGO_URI) {
+      throw new Error('MONGO_URI is not defined in the environment variables');
+    }
+
+    // Connect to MongoDB
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      dbName:"olynex-managements" //! database name
+      dbName: 'olynex-managements' // Database name
     });
+
     console.log('MongoDB connected...');
   } catch (err) {
-    console.error(err.message);
+    console.error('Error connecting to MongoDB:', err.message);
     process.exit(1);
   }
 };
