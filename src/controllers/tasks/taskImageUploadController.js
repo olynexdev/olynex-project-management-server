30478a767871a2840f6595b506931206b0ed2c35
OSL28 <olynexdevteam@gmail.com>
2024-09-09 17:48:31 +0600

    checked

--- src/controllers/tasks/taskImageUploadController.js
+++ src/controllers/tasks/taskImageUploadController.js
@@ -4,17 +4,14 @@ const path = require('path');
 // Set up multer for file storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    console.log("cb", cb, file);
     cb(null, '/var/www/html/uploads'); // Save files to the "uploads" folder
   },
   filename: (req, file, cb) => {
-    console.log("this is a file:",file);
     cb(null, Date.now() + path.extname(file.originalname)); // Append timestamp to filename
   },
 });
 
 const upload = multer({ storage });
-console.log("upload", upload);
 
 // Route to handle image uploads
 const postImage = (req, res) => {
@@ -22,7 +19,6 @@ const postImage = (req, res) => {
     const imageUrls = req.files.map(
       file => `${req.protocol}://${req.hostname}/uploads/${file.filename}`
     );
-    console.log(imageUrls);
     res.status(200).json({ imageUrls });
   } catch (error) {
     console.error('Image upload error:', error);
